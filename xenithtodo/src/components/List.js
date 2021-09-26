a145bc783da6f1e416e40ee2be811f07e2d7b9a7
tommyedmunds <73081204+tommyedmunds@users.noreply.github.com>
2021-09-26 18:36:03 -0500

    list is now rendering, still much to do regarding styling. list items are very simple components made of text only

---
+++ xenithtodo/src/components/List.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Item from './Item';
+
+export default function List(props) {
+  const itemsArr = [];
+
+  const itemsRender = () => {};
+
+  return 'a';
+}
--- xenithtodo/src/components/ListCreator.js
+++ xenithtodo/src/components/ListCreator.js
@@ -1,19 +1,72 @@
-import react from 'react';
+import React from 'react';
+import { useState } from 'react';
+import Item from './Item';
+import List from './List';
 
 export default function ListCreator(props) {
+  const [inputVal, setInputVal] = useState('');
+  const [listArr, setListArr] = useState([]);
+
+  const addNewItem = (val) => {
+    setListArr((listArr) => [...listArr, val]);
+
+    console.log(listArr);
+    return listArr;
+  };
+
+  const handleInputClick = (e) => {
+    e.target.value = '';
+    e.target.style.opacity = 1;
+    setInputVal(e.target.value);
+    //console.log(inputVal);
+  };
+
+  const handleInputChange = (e) => {
+    //console.log(e.target.value);
+    const a = e.target.value;
+    setInputVal(a);
+    console.log(a, inputVal);
+  };
+
+  const handleInputSubmit = (e) => {
+    e.preventDefault();
+    addNewItem(inputVal);
+    e.target.children[0].children[0].value = '';
+  };
+
+  const renderItems = () => {
+    return listArr.map((i) => {
+      return <Item text={i} />;
+    });
+  };
+
   return (
-    <div className='App-header'>
-      <div className='header'>
-        <h1>T O D O</h1>
-        <div className='inputBox'>
-          <span className='dot'></span>
-          <form className='inputField'>
-            <label>
-              <input defaultValue='Create a new item...' type='text' />
-            </label>
-          </form>
+    <div>
+      <div className='App-header'>
+        <div className='header'>
+          <h1>T O D O</h1>
+          <div className='inputBox'>
+            <span className='dot'></span>
+            <form className='inputField' onSubmit={handleInputSubmit}>
+              <label>
+                <input
+                  defaultValue='Create a new item...'
+                  type='text'
+                  onClick={handleInputClick}
+                  onChange={handleInputChange}
+                  style={{ color: '#FFFFFF', opacity: 0.4 }}
+                />
+              </label>
+            </form>
+          </div>
         </div>
       </div>
+      <div className='space'></div>
+      <center>
+        <div className='List'>
+          <div className='ListDiv'>{renderItems()}</div>
+        </div>
+      </center>
     </div>
   );
 }
